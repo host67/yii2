48fb2a9a046e73d451112f2fef6248ba4ab29fef
host67 <host67@yandex.ru>
2023-11-08 11:13:15 +0300

    Update model Product and model Image

--- common/models/Product.php
+++ common/models/Product.php
@@ -6,4 +6,28 @@ use yii\db\ActiveRecord;
 
 class Product extends ActiveRecord
 {
+    public static function tableName() {
+        return 'product';
+    }
+    
+    public function rules() {
+        return [
+            [['name', 'price'], 'required'],
+            [['name'], 'string'],
+            [['price'], 'double'],
+            [['image_id'], 'integer']
+        ];
+    }
+    
+    public function attributeLabels() {
+        return [
+            'id' => 'ID',
+            'name' => 'Название',
+            'price' => 'Цена'
+        ];
+    }
+    
+    public function getImage() {
+        return $this->hasOne(Image::class, ['id' => 'image_id']);
+    }
 }
